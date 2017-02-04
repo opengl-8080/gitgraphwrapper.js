b833bfbb154bea08e986745a7c5fbe9b84ef7a03
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-02-04 09:52:37 +0900

    add comment

--- usage/usage.js
+++ usage/usage.js
@@ -1,22 +1,33 @@
 new GitGraphWrapper({orientation: 'vertical-reverse'})
+    // create 'master' branch and checkout it.
     .branch('master')
     .checkout('master')
     .commit()
     .commit()
     .tag('v1.0')
+
+    // create 'develop' branch and checkout it.
     .branch('develop')
     .checkout('develop')
     .commit('Hello')
     .tag('tag2')
     .commit()
+
+    // checkout master
     .checkout('master')
     .commit()
+
+    // create orphan branch 'orphan' and checkout it.
     .orphanBranch('orphan')
     .checkout('orphan')
     .commit()
     .commit()
+
+    // checkout 'develop'
     .checkout('develop')
     .commit()
+
+    // checkout 'master' and merge 'develop' and 'orphan' branches.
     .checkout('master')
     .merge('develop', {message: 'merge from develop'})
     .merge('orphan')

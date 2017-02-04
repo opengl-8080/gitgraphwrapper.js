e437f26ac20c8374ac92f0f073dc67b18fcbd5f8
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-02-04 09:40:11 +0900

    refactoring that using prototype

--- src/gitgraphwrapper.js
+++ src/gitgraphwrapper.js
@@ -1,38 +1,42 @@
-function GitGraphWrapper() {
-    this.gitGraph = new GitGraph();
-    this.branches = {};
-    this.head = undefined;
+(function() {
+    function GitGraphWrapper() {
+        this.gitGraph = new GitGraph();
+        this.branches = {};
+        this.head = undefined;
+    }
 
-    this.branch = function(option) {
+    GitGraphWrapper.prototype.branch = function(option) {
         var createdBranch = this.gitGraph.branch(option);
         this.branches[createdBranch.name] = createdBranch;
         return this;
     };
 
-    this.checkout = function(branchName) {
+    GitGraphWrapper.prototype.checkout = function(branchName) {
         this.head = this.branches[branchName];
         this.head.checkout();
         return this;
     };
 
-    this.commit = function(option) {
+    GitGraphWrapper.prototype.commit = function(option) {
         this.gitGraph.commit(option);
         return this;
     };
 
-    this.tag = function(option) {
+    GitGraphWrapper.prototype.tag = function(option) {
         this.gitGraph.tag(option);
         return this;
     };
 
-    this.orphanBranch = function(option) {
+    GitGraphWrapper.prototype.orphanBranch = function(option) {
         var createdBranch = this.gitGraph.orphanBranch(option);
         this.branches[createdBranch.name] = createdBranch;
         return this;
     };
 
-    this.merge = function(targetBranchName, option) {
+    GitGraphWrapper.prototype.merge = function(targetBranchName, option) {
         this.branches[targetBranchName].merge(this.head, option);
         return this;
     };
-}
+
+    window.GitGraphWrapper = GitGraphWrapper;
+})();
